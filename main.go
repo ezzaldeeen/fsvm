f5c71be25de90d50135e2e1a2f175644084ee37f
ezzaldeeen <ezzaldeeenmousa@gmail.com>
2023-07-29 18:46:02 +0300

    refine entry point

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
-func main() {
-	//kv := make(map[string]eventstore.Events)
-	//storage := eventstore.NewEventStore(kv)
-	//svc := order.NewService(storage)
+import (
+	"github.com/ezzaldeeen/fsvm/eventstream"
+	"github.com/ezzaldeeen/fsvm/machine"
+	"log"
+	"sync"
+)
 
-	//svc.SelectProduct()
+const numOfGoroutines = 2
+
+func main() {
+	wg := new(sync.WaitGroup)
+	// initialize vending machine
+	mac := machine.NewMachine()
+	// channel for event propagation
+	events := make(chan eventstream.Event)
+	// dispatcher for the occurred events
+	dispatcher := eventstream.NewEventDispatcher(mac, events)
+	// handler for handle the dispatched events
+	handler := eventstream.NewEventHandler(mac, events)
+
+	wg.Add(numOfGoroutines)
+	go dispatcher.Run()
+	go func() {
+		err := handler.Handle()
+		if err != nil {
+			log.Println(err)
+		}
+	}()
+	wg.Wait()
 }
